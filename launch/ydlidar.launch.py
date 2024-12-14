4db72360665035bf50bb0ea8c6ce6242f33f6b54
angyiherng <yiherng.ang0618@gmail.com>
2024-12-14 17:26:35 +0800

    edit

---
+++ launch/ydlidar.launch.py
@@ -0,0 +1,19 @@
+import os
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='ydlidar_ros2_driver-master',
+            executable='ydlidar_ros2_driver_node',
+            output='screen',
+            parameters=[{
+                'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.2:1.0-port0',
+                'frame_id': 'laser_frame',
+                'angle_composition': 'True',
+                'scan_mode': 'Standard'
+            }]
+        )
+    ])
