e06141355245026f8be68c96552ef6dbf23dc962
angyiherng <yiherng.ang0618@gmail.com>
2024-12-14 16:49:55 +0800

    Add Camera launch file

---
+++ launch/camera.launch.py
@@ -0,0 +1,22 @@
+import os 
+
+from launch import LaunchDescription
+from lauch_ros.actions import Node
+
+def generate_launch_description():
+
+    return LaunchDescription([
+
+        Node(
+            package='v412_camera',
+            executable='v412_camera_node',
+            output='screen',
+            namespace='camera',
+            parameters=[{
+                'image_size': [640,480],
+                'time_per_frame': [1,6],
+                'camera_frame_id': 'camera_link_optical'
+            }]
+
+        )
+    ])
