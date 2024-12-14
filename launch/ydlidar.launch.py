ca3f25d536e5b37860c03289207a7a009055ee4c
angyiherng <yiherng.ang0618@gmail.com>
2024-12-14 17:33:35 +0800

    edit

--- launch/ydlidar.launch.py
+++ launch/ydlidar.launch.py
@@ -1,19 +1,42 @@
-import os
+from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
+from launch_ros.actions import LifecycleNode
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.actions import LogInfo
+
+import lifecycle_msgs.msg
+import os
+
 
 def generate_launch_description():
+    share_dir = get_package_share_directory('ydlidar_ros2_driver')
+    parameter_file = LaunchConfiguration('params_file')
+    node_name = 'ydlidar_ros2_driver_node'
+
+    params_declare = DeclareLaunchArgument('params_file',
+                                           default_value=os.path.join(
+                                               share_dir, 'params', 'ydlidar_x3.yaml'),
+                                           description='FPath to the ROS2 parameters file to use.')
+
+    driver_node = LifecycleNode(package='ydlidar_ros2_driver',
+                                node_executable='ydlidar_ros2_driver_node',
+                                node_name='ydlidar_ros2_driver_node',
+                                output='screen',
+                                emulate_tty=True,
+                                parameters=[parameter_file],
+                                node_namespace='/',
+                                )
+    tf2_node = Node(package='tf2_ros',
+                    node_executable='static_transform_publisher',
+                    node_name='static_tf_pub_laser',
+                    arguments=['0', '0', '0.02','0', '0', '0', '1','base_link','laser'],
+                    )
+
     return LaunchDescription([
-        Node(
-            package='ydlidar_ros2_driver',
-            executable='ydlidar_ros2_driver_node',
-            output='screen',
-            parameters=[{
-                'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.2:1.0-port0',
-                'frame_id': 'laser_frame',
-                'angle_composition': 'True',
-                'scan_mode': 'Standard'
-            }]
-        )
+        params_declare,
+        driver_node,
+        #tf2_node,
     ])
