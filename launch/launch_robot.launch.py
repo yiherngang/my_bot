fe9a51c432c9fb94321843f710b3ed158ce70fd0
angyiherng <yiherng.ang0618@gmail.com>
2024-12-15 16:28:52 +0800

    edit

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -28,11 +28,11 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
     )
 
-    #joystick = IncludeLaunchDescription(
-    #            PythonLaunchDescriptionSource([os.path.join(
-    #                get_package_share_directory(package_name),'launch','joystick.launch.py'
-    #            )]), launch_arguments={'use_sim_time': 'true'}.items()
-    #)
+    joystick = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','joystick.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
 
     #twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
     #twist_mux = Node(
