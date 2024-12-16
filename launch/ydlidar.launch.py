0f32873442c2951e12cf5972616e7eb6f5bd698b
angyiherng <yiherng.ang0618@gmail.com>
2024-12-16 11:10:56 +0800

    update

--- launch/ydlidar.launch.py
+++ launch/ydlidar.launch.py
@@ -1,3 +1,17 @@
+#!/usr/bin/python3
+# Copyright 2020, EAIBOT
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
@@ -13,6 +27,7 @@ import os
 
 def generate_launch_description():
     share_dir = get_package_share_directory('ydlidar_ros2_driver')
+    rviz_config_file = os.path.join(share_dir, 'config','ydlidar.rviz')
     parameter_file = LaunchConfiguration('params_file')
     node_name = 'ydlidar_ros2_driver_node'
 
@@ -29,9 +44,20 @@ def generate_launch_description():
                                 parameters=[parameter_file],
                                 node_namespace='/',
                                 )
+    tf2_node = Node(package='tf2_ros',
+                    node_executable='static_transform_publisher',
+                    node_name='static_tf_pub_laser',
+                    arguments=['0', '0', '0.02','0', '0', '0', '1','base_link','laser'],
+                    )
+    rviz2_node = Node(package='rviz2',
+                    node_executable='rviz2',
+                    node_name='rviz2',
+                    arguments=['-d', rviz_config_file],
+                    )
 
     return LaunchDescription([
         params_declare,
         driver_node,
-        #tf2_node,
+        tf2_node,
+        rviz2_node,
     ])
