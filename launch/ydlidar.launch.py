9c64b908ed5adf318eb5b2680ac084c670f16e7f
angyiherng <yiherng.ang0618@gmail.com>
2024-12-16 11:20:18 +0800

    update

--- launch/ydlidar.launch.py
+++ launch/ydlidar.launch.py
@@ -14,14 +14,8 @@ import os
 
 def generate_launch_description():
     share_dir = get_package_share_directory('ydlidar_ros2_driver')
-    rviz_config_file = os.path.join(share_dir, 'config','ydlidar_x3.rviz')
     parameter_file = LaunchConfiguration('params_file')
     node_name = 'ydlidar_ros2_driver_node'
-
-    params_declare = DeclareLaunchArgument('params_file',
-                                           default_value=os.path.join(
-                                               share_dir, 'params', 'ydlidar_x3.yaml'),
-                                           description='FPath to the ROS2 parameters file to use.')
 
     driver_node = LifecycleNode(package='ydlidar_ros2_driver',
                                 node_executable='ydlidar_ros2_driver_node',
@@ -33,6 +27,5 @@ def generate_launch_description():
                                 )
 
     return LaunchDescription([
-        params_declare,
         driver_node,
     ])
